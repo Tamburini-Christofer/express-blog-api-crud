65933df89a23499f4daf05edfc239464fb9dd079
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-17 14:22:10 +0200

    Reset + creazione funzione index

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,44 +1,18 @@
-//todo Importo Express
-const express = require ("express");
+//todo Importo i post
+const posts = require("../data/postsArray");
 
-//todo Definisco il router
-const router = express.Router();
+function index(req, res) {
 
-//todo Recuperiamo i posts
-const Post = require('../data/posts');
+    let filterPost = posts;
 
-//?INDEX
-const index = (req, res) => {
-    res.json(Post);
-}
+    if (req.query.tags) {
+        filterPost = posts.filter(
+           post => post.tags.includes(req.query.tags)
+        );
+    }
 
-//?SHOW
-const show = (req, res) => {
-    //todo Recuperiamo l'id dai parametri della richiesta
-    const id = parseInt(req.params.id);
-    //todo Cerchiamo il post con l'id corrispondente
-    const post = Post.find(p => p.id === id);   
-    //todo Rispondiamo con il post trovato
-    res.json(post);
-}
+    res.json(filterPost);
 
-//?STORE
-const store = (req, res) => {
-    res.json("Creazione di un post")
-}
-
-//?UPDATE 
-const update = (req, res) => {
-
-}
-
-//?MODIFY
-const modify = (req, res) => {
-
-}
-
-//?DESTROY
-const destroy = (req, res) => {
 
 }
 
