d1e7726dcb21f3d3cab417c6920ce46b3f3f9452
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-17 14:24:46 +0200

    Creazione funzione show

--- controllers/controller.js
+++ controllers/controller.js
@@ -16,3 +16,17 @@ function index(req, res) {
 
 }
 
+function show(req, res) {
+    const id = parseInt(req.params.id)
+    const post = posts.find(post => post.id === id);
+
+    if (!post) {
+        res.status(404)
+        return res.json({
+            error: "Not Found",
+            message: "Post non trovato"
+        })
+    }
+    res.json(post);
+}
+
