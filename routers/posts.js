a0d1b3849fb8f66d0bc9c5b50ef035cc09e7d4b6
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-17 14:32:55 +0200

    Inserimento controller in posts.js

---
+++ routers/posts.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const router = express.Router();
+
+const postsController = require('../controllers/contoller');
+
+//?INDEX
+router.get('/', postsController.index);
+
+//?SHOW
+router.get('/:id', postsController.show);
+
+//?STORE
+router.post('/', postsController.store);
+
+//?UPDATE
+router.put('/:id', postsController.update);
+
+//?MODIFY
+router.patch('/:id', postsController.modify);
+
+//?DESTROY
+router.delete('/:id', postsController.destroy);
+
+module.exports = router;
