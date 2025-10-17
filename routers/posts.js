903b30c89a2caa7d5dabe96b3a13e279acb39937
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-17 14:48:56 +0200

    Test di funzionamento Postman

--- routers/posts.js
+++ routers/posts.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const postsController = require('../controllers/contoller');
+const postsController = require('../controllers/controller');
 
 //?INDEX
 router.get('/', postsController.index);
